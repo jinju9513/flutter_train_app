5e56f1a94cfce6c587bc805273a9c77fdf4b5c58
jinju9513 <wjswlswn99@naver.com>
2025-03-31 11:33:27 +0900

    feat: stationListPage 리스트 출력

--- lib/pages/station/station_list_page.dart
+++ lib/pages/station/station_list_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class StationListPage extends StatelessWidget{
   final String title;
+  final List<String> stations = ["수서","동탄","평택지제","천안아산","오송","대전","김천구미","동대구","경주","울산","부산"];
 
   StationListPage({required this.title});
   @override
@@ -10,6 +11,16 @@ class StationListPage extends StatelessWidget{
     appBar: AppBar(
       title: Text(title),
     ),
+    body: ListView.builder(
+      itemCount: stations.length,
+      itemBuilder: (context,index){
+        return ListTile(
+          title: Text(stations[index]),
+          onTap: () {
+            print('${stations[index]} 선택됨');
+          },
+        );
+      }),
     
    );
   }
