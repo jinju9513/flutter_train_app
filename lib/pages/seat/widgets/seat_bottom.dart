27b93fa857d6a3c9c693c2ac286adefbac6a77fd
jinju9513 <wjswlswn99@naver.com>
2025-04-01 12:36:36 +0900

    feat: seat bottom 다이얼로그 구현

--- lib/pages/seat/widgets/seat_bottom.dart
+++ lib/pages/seat/widgets/seat_bottom.dart
@@ -1,24 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SeatBottom extends StatelessWidget{
+class SeatBottom extends StatelessWidget {
+  final int? selectedSeatIndex;
+
+  const SeatBottom({required this.selectedSeatIndex});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      child: ElevatedButton(onPressed: (){},
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.purple,
-        foregroundColor: Colors.white,
-         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-      ), 
-      child: Text('예매 하기',
-      style: TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 0, 16, 32),
+      child: SizedBox(
+        width: double.infinity,
+        height: 55,
+        child: ElevatedButton(
+          onPressed: () {
+            if (selectedSeatIndex == null) {
+              return;
+            }
+            // 선택된 좌석 번호 계산
+            int row = (selectedSeatIndex! ~/ 4) + 1; // 0~9 => 1~10
+            int col = selectedSeatIndex! % 4;
+            String colLabel = ['A', 'B', 'C', 'D'][col]; //배역에서 꺼내기
+            String seatLabel = '$row-$colLabel';
+
+            showCupertinoDialog(
+              context: context,
+              builder:
+                  (context) => CupertinoAlertDialog(
+                    title: Text('예매를 진행할까요?'),
+                    content: Text('선택한 좌석:$seatLabel'),
+                    actions: [
+                      CupertinoDialogAction(
+                        child: Text('취소'),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      CupertinoDialogAction(
+                        child: Text('확인'),
+                        onPressed: () {
+                          Navigator.pop(context); //다이얼로그 닫기
+                          Navigator.pop(context); //SeatPage 닫기
+                          Navigator.pop(context); //HomePage 돌아가기
+                        },
+                      ),
+                    ],
+                  ),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.purple,
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          child: Text(
+            '예매 하기',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
-      )),
     );
   }
 }
